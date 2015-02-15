e6fe35dfd8af0bbb8a0a6eac994f86459c25d8a4
Joseph Mansfield <sftrabbit@gmail.com>
2015-02-15 15:47:38 +0000

    Add shorthand for cppreference links

--- _plugins/sample_markdown.rb
+++ _plugins/sample_markdown.rb
@@ -6,10 +6,16 @@ module CppSamples
 
 		def sample_description(sample)
 			description = sample['description']
+
 			description.gsub!(/\[(\d+)\]/) do |match|
 				line_num = $1.to_i - sample['code_offset']
 				"<span class=\"lineref\" data-line=\"#{line_num}\">line #{line_num}</span>"
 			end
+
+			description.gsub!(/\[(.+?)\]\((c(pp)?\/.+?)\)/) do |match|
+				"<a href=\"http://en.cppreference.com/w/#{$2}\">#{$1}</a>"
+			end
+
 			markdownify(description)
 		end
 	end
